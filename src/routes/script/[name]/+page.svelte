dbd19bf3bea04a5e5a62a338923e62ba24083a1e
Andrew Feltham <andrew.feltham3@gmail.com>
2025-04-27 13:26:30 -0600

    fix: fix the script details page to have stick top correctly

--- src/routes/script/[name]/+page.svelte
+++ src/routes/script/[name]/+page.svelte
@@ -18,7 +18,7 @@
 	<article class="card mx-auto max-w-screen-lg bg-base-100 p-12 shadow-sm">
 		<PluginHeader {comicRackScript} />
 
-		<div class="my-0 mb-12 grid grid-cols-5 gap-16">
+		<div class="my-0 mt-6 mb-12 grid grid-cols-5 gap-16">
 			<div class="col-span-3">
 				<PluginAbout {comicRackScript} />
 				<div class="mt-8">
@@ -26,7 +26,7 @@
 				</div>
 			</div>
 
-			<div class="sticky top-10 col-span-2">
+			<div class="sticky top-10 col-span-2 self-start">
 				<SidebarInfo {comicRackScript} />
 			</div>
 		</div>
