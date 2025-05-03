f6e47f43d88ba81ce15fde5d152dc5a120d53c57
Andrew Feltham <andrew.feltham3@gmail.com>
2025-05-03 16:41:38 -0600

    feat: add better errors when validating the scripts

--- codegen/validate.ts
+++ codegen/validate.ts
@@ -1,16 +1,39 @@
+import chalk from 'chalk';
 import { loadScripts } from './load-scripts';
 import { isValidScript } from './script-validator';
 
 async function validateAllScripts() {
+	let hasErrors = false;
+
+	const errors: Record<string, string[]> = {};
+
 	for await (const { data, fileName } of loadScripts()) {
+		process.stdout.write(`${fileName} `);
+
 		if (!isValidScript(data)) {
-			console.error(`Invalid script: ${fileName}`);
-			console.error(JSON.stringify(isValidScript.errors));
-			return;
+			console.log(chalk.red(`Invalid!`));
+			errors[fileName] =
+				isValidScript.errors?.map((error) => `${error.instancePath} ${error.message}`) ?? [];
+			hasErrors = true;
+		} else {
+			process.stdout.write(chalk.green(`Valid!\n`));
 		}
 	}
 
-	console.log('All scripts are valid');
+	if (hasErrors) {
+		console.log(chalk.red('\nErrors:'));
+
+		for (const [fileName, errorMessages] of Object.entries(errors)) {
+			console.log(`\n${fileName}: `);
+			for (const error of errorMessages) {
+				console.log(`\t${error}`);
+			}
+		}
+
+		process.exit(1);
+	}
+
+	console.log(chalk.green('All scripts are valid'));
 }
 
 await validateAllScripts();
